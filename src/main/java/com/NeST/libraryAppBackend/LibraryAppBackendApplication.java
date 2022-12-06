38e6a7af5a5b0368c00359d8e6512d272fc92419
Salooma <saloomaali2000@gmail.com>
2022-12-06 08:01:19 +0530

    nw backend project

---
+++ src/main/java/com/NeST/libraryAppBackend/LibraryAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.NeST.libraryAppBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryAppBackendApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/NeST/libraryAppBackend/controller/libraryController.java
@@ -0,0 +1,14 @@
+package com.NeST.libraryAppBackend.controller;
+
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class libraryController {
+
+    @PostMapping("/")
+    public String libraryWelcome(){
+        return "welcome to library app";
+    }
+}
